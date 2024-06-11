5dc183efb1dad0babee512233a7de7712c6bc253
1302harsh <1302harsh@gmail.com>
2024-06-11 19:56:48 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const studentName = "Harsh";
+const studentNumber = "1163059";
+console.log(`${studentName}- ${studentNumber}`);
+
+let headerContent = document.querySelector(`h1`);
+headerContent.classList.add(`heading1`); 
+headerContent.innerHTML = `${studentName}- ${studentNumber}`;
+
+
+
